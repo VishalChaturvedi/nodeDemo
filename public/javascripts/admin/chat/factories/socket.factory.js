2603c8278364311a880a968db9441387a0a19c49
vishal <vishal.chaturvedi@systematix.com>
2016-09-21 12:57:41 +0530

    depoly socket based chat app

--- public/javascripts/admin/chat/factories/socket.factory.js
+++ public/javascripts/admin/chat/factories/socket.factory.js
@@ -22,7 +22,7 @@
      */
     function SocketFactory($http, $location, $rootScope)
     {
-        var socket = io.connect("http://localhost:5000");
+        var socket = io.connect("127.0.0.1:5000");
         return {
             on: function(eventName, callback){
                 socket.on(eventName, callback);
